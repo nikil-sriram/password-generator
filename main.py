05aba1dc04714261df3e48cba75744a8ef0d4f11
Nikil <nikilsriram123@gmail.com>
2023-12-31 16:48:06 -0800

    Prints the amount of characters we want

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import random
 amountofpasswords = int(input("How many passwords would you like?"))
 amountofcharacters = int(input("How many characters would you like?"))
-chars = ["qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM!@#$%^&*()"]
+chars = "q Q w W e E r R t T y Y u U i I o O p P a A s S d D f F g G h H j J k K l L z Z x X c C v V b B n N m M ! @ # $ % ^ * ()"
+
+charssplitted = chars.split()
 
 passwords = [[] for i in range(amountofpasswords)]
 
+testing = random.sample(charssplitted, amountofcharacters)
+print(testing)
