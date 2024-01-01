fc2f3b42272a03eb753c0feacac669fda8dca1da
Nikil <nikilsriram123@gmail.com>
2023-12-31 16:39:13 -0800

    Created basic parts of the app
    Created the inputs for amount of passwords and characters. Put all the characters we are going to use in a list. And then making a passwords list.

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+amountofpasswords = int(input("How many passwords would you like?"))
+amountofcharacters = int(input("How many characters would you like?"))
+chars = ["qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM!@#$%^&*()"]
+
+passwords = [[] for i in range(amountofpasswords)]
+
